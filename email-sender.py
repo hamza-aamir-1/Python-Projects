674adbadb3754a030ece4f6e0dba0c08d1185c05
Hamza Aamir <hamzaamirm2001@gmail.com>
2023-01-17 22:16:57 +0500

    Email Sent Successfully

--- email-sender.py
+++ email-sender.py
@@ -1,14 +1,22 @@
 from email.message import EmailMessage
 import password
+import ssl
+import smtplib
 
 emailSender = "hamzaamirm2001@gmail.com"
 passSender = password.passKey()
-emailReceiver = ""
-subject = ""
-body = ""
+emailReceiver = "yesago1323@usharer.com"
+subject = "Hello World!"
+body = "This is an auto generated email just for testing a program."
 
 em = EmailMessage()
 em['From'] = emailSender
-em['To'] = ''
+em['To'] = emailReceiver
 em['Subject'] = subject
 em.set_content(body)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(emailSender, passSender)
+    smtp.sendmail(emailSender, emailReceiver, em.as_string())
